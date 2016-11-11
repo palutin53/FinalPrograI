fe7edcd0cc9fbf8541ef87d2b989c2e73256c48c
palutin53 <palutin31@gmail.com>
2016-11-11 11:36:24 -0600

    update11:3611112016

---
+++ Proceso.java
@@ -0,0 +1,30 @@
+/**
+* Descripción de la aplicacióon
+*
+* @author 
+* @version 1.0.0
+*/
+
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Proceso
+{
+
+	private static String getInput(String prompt)
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.print(prompt);
+		System.out.flush();
+		try{
+			return br.readLine();
+		}catch(Exception e){
+			return "\nError: " + e.getMessage();
+		}
+
+	}
+	public static void print(String s)
+	{
+		System.out.print(s);
+	}
+}
