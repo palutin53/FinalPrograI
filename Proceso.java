ecb858f4d0a1eed4674d2e0ad2cf5e629af2a6be
palutin53 <palutin31@gmail.com>
2016-11-11 12:01:47 -0600

    update12:0111112016

--- Proceso.java
+++ Proceso.java
@@ -10,7 +10,17 @@ import java.io.BufferedReader;
 
 public class Proceso
 {
-
+	public static void arreglos()
+	{
+		String[] alumnos = new String[10];
+		String[] docentes = new String[5];
+		String[] cursost1 = new String[4]; 
+		String[] cursost2 = new String[4];
+		String[] cursost3 = new String[4];
+		String[] cursost4 = new String[4];
+		float[] notas = new float[89];
+		String[] pagos = new String[10];
+	}
 	private static String getInput(String prompt)
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
