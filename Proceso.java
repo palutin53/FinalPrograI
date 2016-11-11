193ac69f058450f2bd3a15e7c30c0bddda8f4ed0
palutin53 <palutin31@gmail.com>
2016-11-11 14:20:58 -0600

    AddSubMenus

--- Proceso.java
+++ Proceso.java
@@ -32,9 +32,19 @@ public class Proceso
 		{
 			print("\n\t\t\tBinenvenido a UDEO");
 			print("\n\n\tIngresa el valor según la opcion que selecciones.");
-			print("\n\n\t\t1.- Administrar Alumnos\n\t\t2.- Administrar Docentes\n\t\t3.- Administrar Cursos\n\t\t4.- Realizar Pago\n\t\t5.- Ingresar Notas\n\t\t6.- Ver Resultados Finales.");
+			print("\n\n\t\t1.- Administrar Alumnos\n\t\t2.- Administrar Docentes\n\t\t3.- Administrar Cursos\n\t\t4.- Realizar Pago\n\t\t5.- Ingresar Notas\n\t\t6.- Ver Resultados Finales.\n\t\t7.- SALIR");
 		}catch(Exception e){
 			print(">>>> ERROR");
+		}
+	}
+	public static void subMenu()
+	{
+		try{
+			print("\n\t\t\tAdministrar");
+			print("\n\n\tIngresa el valor según la opción que selecciones.");
+			print("\n\n\t\t1.- Agregar\n\t\t2.- Editar\n\t\t3.- Eliminar\n\t\t4.- VOLVER");
+		}catch(Exception e){
+			print(">>> Error");
 		}
 	}
 }
--- Program.java
+++ Program.java
@@ -17,44 +17,77 @@ public class Program
 		String[] pagos = new String[10];
 		//Delcaracion variables temporales
 		String user, pass;
-		int contador=0;
-		boolean flag=true;
+		int contador = 0;
+		boolean flag = true, valid = true, validsub = true;
 		//Declaracion metodo de procesos
 		Proceso billy = new Proceso();
 
 		try
 		{
 			//Login
-			while(flag && contador<3){
+			while(flag && contador<3)
+			{
 				user = billy.getInput("\nIngresa tu usuario: ");
 				pass = billy.getInput("Ingrese su contraseña: ");
 				if(user.equals(USERV) && pass.equals(PASSV))
 				{
-					billy.print("Has ingresado\n");
-					flag=false;
+					billy.print("¡Bienvenido!\n");
+					flag = false;
 					//Menu Principal
+
 					billy.menuPrin();
 					int opcion = Integer.parseInt(billy.getInput("\n\n\t\tTu Opción: "));
 
-					switch(opcion)
+					// Validacion para salida
+
+					while(valid)
 					{
-						case 1:
-						break;
-						case 2:
-						break;
-						case 3:
-						break;
-						case 4:
-						break;
-						case 5:
-						break;
-						default:
-						break;
+						switch(opcion)
+						{
+							case 1:
+									// Administrar Alumnos
+									billy.subMenu();
+									int subOpcion = Integer.parseInt(billy.getInput("\n\n\tIngresa tu opcion: "));
+									switch(subOpcion)
+									{
+										case 1:
+										break;
+										case 2:
+										break;
+										case 3:
+										break;
+										case 4:
+										break;
+										default:
+										break;
+									}
+							break;
+							case 2:
+								// Administrar Docentes
+								billy.subMenu();
+							break;
+							case 3:
+								// Administrar Cursos
+								billy.subMenu();
+							break;
+							case 4:
+							break;
+							case 5:
+							break;
+							case 6:
+							break;
+							case 7:
+								billy.print("\n\n\t\t\tSaliendo del programa.");
+								valid = false;
+							break;
+							default:
+							break;
+						}
 					}
 
 				}else{
 					contador++;
-					billy.print("\nIntenta de nuevo");
+					billy.print("\nIntenta de nuevo.");
 				}
 				
 			}
