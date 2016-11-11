b11be4950fee2f23cf74ab22681206379b48bceb
palutin53 <palutin31@gmail.com>
2016-11-11 16:08:03 -0600

    Menu Funcionando al 100%

--- Program.java
+++ Program.java
@@ -15,10 +15,11 @@ public class Program
 		String[] cursost4 = new String[4];
 		float[] notas = new float[89];
 		String[] pagos = new String[10];
+		float nota1 = 0, nota2 = 0, nota3 = 0, nota4 = 0, nota5 = 0, nota6 = 0, parcial = 0, proyecto = 0, examenFinal = 0;
 		//Delcaracion variables temporales
 		String user, pass;
 		int contador = 0;
-		boolean flag = true, valid = true, validsub = true;
+		boolean flag = true, valid = true;
 		//Declaracion metodo de procesos
 		Proceso billy = new Proceso();
 
@@ -33,54 +34,130 @@ public class Program
 				{
 					billy.print("¡Bienvenido!\n");
 					flag = false;
-					//Menu Principal
-
-					billy.menuPrin();
-					int opcion = Integer.parseInt(billy.getInput("\n\n\t\tTu Opción: "));
-
 					// Validacion para salida
-
+					//Menu Principal
 					while(valid)
 					{
+						billy.menuPrin();
+						int opcion = Integer.parseInt(billy.getInput("\n\n\t\tTu Opción: "));
+
+						boolean validsub = true;
 						switch(opcion)
 						{
 							case 1:
-									// Administrar Alumnos
-									billy.subMenu();
-									int subOpcion = Integer.parseInt(billy.getInput("\n\n\tIngresa la opcion: "));
-									switch(subOpcion)
+									while(validsub)
 									{
-										case 1:
-										break;
-										case 2:
-										break;
-										case 3:
-										break;
-										case 4:
-										break;
-										default:
-										break;
+										// Administrar Alumnos
+										billy.subMenu();
+										int subOpcion = Integer.parseInt(billy.getInput("\n\n\tIngresa la opcion: "));
+										switch(subOpcion)
+										{
+											case 1:
+												// Agregar
+											break;
+											case 2:
+												// Modificar
+											break;
+											case 3:
+												// Eliminar
+											break;
+											case 4:
+												// Volver al menú principal
+												billy.print("volviendo al menú princial...");
+												validsub = false;
+											break;
+											default:
+												billy.print("Selección inválida, intentalo de nuevo.");
+												validsub = true;
+											break;
+										}
 									}
 							break;
 							case 2:
-								// Administrar Docentes
-								billy.subMenu();
+								while(validsub)
+									{
+										// Administrar Docentes
+										billy.subMenu();
+										int subOpcion = Integer.parseInt(billy.getInput("\n\n\tIngresa la opcion: "));
+										switch(subOpcion)
+										{
+											case 1:
+												// Agregar
+											break;
+											case 2:
+												// Modificar
+											break;
+											case 3:
+												// Eliminar
+											break;
+											case 4:
+												// Volver al menú principal
+												billy.print("volviendo al menú princial...");
+												validsub = false;
+											break;
+											default:
+												billy.print("Selección inválida, intentalo de nuevo.");
+												validsub = true;
+											break;
+										}
+									}
 							break;
 							case 3:
-								// Administrar Cursos
-								billy.subMenu();
+								while(validsub)
+									{
+										// Administrar Cursos
+										billy.subMenu();
+										int subOpcion = Integer.parseInt(billy.getInput("\n\n\tIngresa la opcion: "));
+										switch(subOpcion)
+										{
+											case 1:
+												// Agregar
+											break;
+											case 2:
+												// Modificar
+											break;
+											case 3:
+												// Eliminar
+											break;
+											case 4:
+												// Volver al menú principal
+												billy.print("volviendo al menú princial...");
+												validsub = false;
+											break;
+											default:
+												billy.print("Selección inválida, intentalo de nuevo.");
+												validsub = true;
+											break;
+										}
+									}
 							break;
 							case 4:
+								// Realizar un pago
 							break;
 							case 5:
+								// Ingreso de Notas
+								billy.print("\n\t\t\t\tIngreso de Notas.");
+								nota1 = Float.parseFloat(getInput("\n\n\tActividad 1:\t\t\t"));
+								nota2 = Float.parseFloat(getInput("\n\tActividad 2:\t\t\t"));
+								nota3 = Float.parseFloat(getInput("\n\tActividad 3:\t\t\t"));
+								nota4 = Float.parseFloat(getInput("\n\tActividad 4:\t\t\t"));
+								nota5 = Float.parseFloat(getInput("\n\tActividad 5:\t\t\t"));
+								nota6 = Float.parseFloat(getInput("\n\tActividad 6:\t\t\t"));
+								parcial = Float.parseFloat(getInput("\n\tExamen Parcial:\t\t\t"));
+								proyecto = Float.parseFloat(getInput("\n\tProyecto:\t\t\t"));
+								examenFinal = Float.parseFloat(getInput("\n\tExamen Final:\t\t\t"));
 							break;
 							case 6:
+								// Resultados Finales
 							break;
 							case 7:
+								// SALIR
 								billy.print("\n\n\t\t\tSaliendo del programa.");
 								valid = false;
 							break;
 							default:
+								billy.print("Selección inválida, intentalo de nuevo.");
+								valid = true;
 							break;
 						}
 					}
